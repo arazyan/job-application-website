96eaa1ecd2a794e0dee184a3744729bcf0f699f2
arazyan <arazdatas@gmail.com>
2026-01-13 23:07:37 +0300

    Add dynamic job listings, API route, 404 handling
    1.Modified the homepage to render job data dynamically using Jinja2 includes.
    2.Added a new JSON API endpoint at /api/jobs.
    3.Implemented a custom error handler for 404 Not Found errors.

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Junior+ Data Analyst",
+        "company": "X5 RETAIL GROUP",
+        "location": "Moscow, office",
+        "salary": "150000 RUB",
+    },
+    {
+        "id": 2,
+        "title": "Middle Backend Developer",
+        "company": "Mail.RU Group",
+        "location": "Moscow, office",
+        "salary": "210000 RUB",
+    },
+    {
+        "id": 3,
+        "title": "Middle+ AQA Python Engineer",
+        "company": "Avito",
+        "location": "Nizhny Novgorod, remote",
+    },
+    {
+        "id": 4,
+        "title": "SDET Python (Senior)",
+        "company": "MAGNIT",
+        "location": "Saint-Petersburg, office",
+        "salary": "250000 RUB",
+    },
+]
+
+
 @app.route(rule="/")
 def get_homepage():
-    return render_template(template_name_or_list="home.html")
+    return render_template(template_name_or_list="home.html", jobs=JOBS)
 
-# @app.route(rule="/")
-def route_not_found():
-    return "<p>Sorry, route not found</p>"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+@app.route(rule="/api/jobs")
+def list_json_jobs():
+    return jsonify(JOBS)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Page not found", 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
